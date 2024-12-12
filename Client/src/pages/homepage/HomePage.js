f773b3d1b2578921bc394b8f75f7053c59d1f815
userocy <167667505+userocy@users.noreply.github.com>
2024-12-12 10:49:58 +0800

    Created and completed HomePage.js
    Has navbar
    google maps api
    section for made trips
    section for discover

---
+++ Client/src/pages/homepage/HomePage.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <nav className="navbar">
+      <div className="navbar-left">
+        <img src="/VISTALOGO.png" className='logo' alt="Your Logo" />
+        <ul className="navbar-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/Itinerary">Itinerary</a></li>
+          <li><a href="/Discover">Discover</a></li>
+          <li><a href="/download-the-app">Download the app</a></li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <span className='wcb'><p>Welcome back,</p>
+        <div className='username'><p>Hanni Pham</p></div></span>
+        <a href='/'>Log out</a>
+        <button className="notification-button">
+            <img src='/bell.png' className='bell' alt='bell'></img>
+          <i className="fa fa-bell"></i>
+          <span className="notification-count">10</span>
+        </button>
+      </div>
+    </nav>
+
+    <section className='MAP'>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5550.585863553436!2d123.91697337622409!3d10.353324797527732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a99894d1b6ae25%3A0xc2d9b9e99316c59d!2sUniversity%20of%20San%20Carlos%20-%20Talamban%20Campus!5e0!3m2!1sen!2sph!4v1733960894276!5m2!1sen!2sph" className='googlemap' border="4px black"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <button><a href='/plantrip'>Plan a new trip +</a></button>
+    </section>
+
+    <section id="current-trips">
+  <h2>Current Trips</h2>
+  <a href='/'>
+  <div class="trip-card">
+    <img src="/skorea.png" alt="South Korea"></img>
+    <h4>Visiting South Korea Temples</h4>
+    <span className='date'>Dec 29 - Dec 31</span>
+  </div>
+  </a>
+  
+  <a href='/'>
+  <div class="trip-card">
+    <img src="/image.png" alt="Taking Friends to my Hometown"></img>
+    <h4>Taking Friends to my Hometown</h4>
+    <span className='date'>Jan 13 - Jan 20</span>
+  </div></a>
+  </section>
+
+  <section id="discover">
+  <h2>Discover</h2>
+  <span className='destination'><p>Discover top destinations, curated based on popular choices by other travelers.</p></span>
+  <div class="destination-card">
+    <img src="/Madrid.png" alt="Madrid"></img>
+    <h4>Madrid</h4>
+    <button>View</button>
+  </div>
+  <div class="destination-card">
+    <img src="/Kyoto.png" alt="Kyoto"></img>
+    <h4>Kyoto</h4>
+    <button>View</button>
+  </div>
+  </section>
+
+
+            
+            <section className='footer'>
+                <img src='/BotDisplay.png' alt='botdisp'></img>
+            </section>
+        </div>
+    );
+};
+
+export default HomePage;
