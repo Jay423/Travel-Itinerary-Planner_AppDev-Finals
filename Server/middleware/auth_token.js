743f6998057c84950d3eab932d7a22cc31179f9c
Ahsec1 <11600056@usc.edu.ph>
2024-12-16 05:47:51 +0800

    token part 2

--- Server/middleware/auth_token.js
+++ Server/middleware/auth_token.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 const authToken = (req, res, next) => {
-    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];  //gets token and split it on the first space
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1]; 
 
     if (!token) {
-        return res.sendStatus(401); //returns unauthorized if there is no token
+        return res.sendStatus(401); 
     }
 
     console.log('Token received:', token);
@@ -12,11 +12,11 @@ const authToken = (req, res, next) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         console.log('Verified User:', user);
         if (err) {
-            return res.sendStatus(403); //returns forbidden if token is invalid
+            return res.sendStatus(403); 
         }
         console.log('Decoded Payload:', user); 
         req.user = user; 
-        next(); //proceeds to next middleware if token is valid
+        next(); 
     });
 };
 
