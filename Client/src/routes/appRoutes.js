9a74ae252d22e055950bb3b1d657e8f2d4a15dd6
Ahsec1 <11600056@usc.edu.ph>
2024-12-16 14:09:04 +0800

    added a 404 error page to the files that don't exist with css

--- Client/src/routes/appRoutes.js
+++ Client/src/routes/appRoutes.js
@@ -1,4 +1,3 @@
-// src/routes/appRoutes.js
 import Home from '../pages/homepage/homePage';
 import Login from '../pages/login/loginUser';
 import Register from '../pages/register/registerUser';
@@ -6,8 +5,7 @@ import Landing from '../pages/landingpage/LandingPage';
 import Calendar from '../pages/calendarapp/calendar';
 import Profile from '../pages/Profile/pfp';
 import TripPlanner from '../pages/tripplanner/TripPlanner';
-
-
+import NotFound from '../components/error';
 
 const appRoutes = [
   { path: '/', component: Landing },
@@ -18,6 +16,7 @@ const appRoutes = [
   { path: '/calendar', component: Calendar },
   { path: '/pfp', component: Profile },
   { path: '/trip', component: TripPlanner },
+  { path: '*', component: NotFound }, 
 ];
 
 export default appRoutes;
